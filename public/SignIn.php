a6f82317ec9e971e681834cab70e8e4a8e24a3aa
Ibnu Naz'm Ar-rosyid <107676258+ibnunazm@users.noreply.github.com>
2023-05-20 17:08:29 +0700

    Add files via upload

---
+++ public/SignIn.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+    <link rel="stylesheet" href="css/style-signin.css">
+</head>
+<body>
+    <div class="container-signin">
+        <div class="signin-text">
+            <h1 class="text">Sign In</h1>
+        </div>
+        <div class="signin-input">
+            <div class="kotak">
+                <input type="text" placeholder="Email">
+                <input type="text" placeholder="Password">
+                <input type="button" class="tombol" value="Sign In">
+                <p><a href="">lupa password?</a></p>
+            </div>
+            <div class="kotak">
+                <p>Belum memiliki akun? <a href="http://localhost:8080/projek-akhir-pemrograman-web/public/SignUp.php">Buat disini</a></p> 
+            </div> 
+        </div>
+    </div>
+</body>
+</html>
+
---
+++ public/SignUp.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/style-signup.css">
+</head>
+<body>
+    <div class="container-signin">
+        <div class="signup-text">
+            <h1 class="text">Sign Up</h1>
+        </div>
+        <div class="signup-input">
+            <div class="kotak">
+                <input type="text" placeholder="Username">
+                <input type="text" placeholder="Email">
+                <input type="text" placeholder="Password">
+                <input type="text" placeholder="Confirm Password">
+                <input type="button" class="tombol" value="Sign Up">
+            </div>
+            <div class="kotak">
+                <p>Sudah memiliki akun? <a href="http://localhost:8080/projek-akhir-pemrograman-web/public/SignIn.php">Login disini</a></p> 
+            </div> 
+        </div>
+    </div>
+</body>
+</html>
+
