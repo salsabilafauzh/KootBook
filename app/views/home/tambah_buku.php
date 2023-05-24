1d3a10ccf76cdbbab16be0f0391990c90f065730
Ahmad Zikry Salam <khadavisibuea@gmail.com>
2023-05-24 21:54:19 +0700

    tambah buku

---
+++ app/views/home/tambah_buku.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="tambah_buku.css" />
+  </head>
+  <body>
+    <div class="contain">
+      <div class="container">
+        <div class="kiri">
+          <div class="cover"></div>
+          <div class="stok">
+            <h1>STOK</h1>
+          </div>
+
+          <div class="isi">
+            <button class="tambah"><h1>+</h1></button>
+            <h1>XXXX</h1>
+            <button class="kurang"><h1>-</h1></button>
+          </div>
+        </div>
+        <div class="kanan">
+          <table border="1" class="detail">
+            <tr>
+              <td id="judul">JUDUL</td>
+              <td>APAKEK</td>
+            </tr>
+            <tr>
+              <td id="judul">PENULIS</td>
+              <td>APAKEK</td>
+            </tr>
+            <tr>
+              <td id="judul">TAHUN TERBIT</td>
+              <td>APAKEK</td>
+            </tr>
+            <tr>
+              <td id="judul">HALAMAN</td>
+              <td>7jt</td>
+            </tr>
+            <tr>
+              <td id="judul">EDISI</td>
+              <td>xxxx</td>
+            </tr>
+            <tr>
+              <td id="judul">BAHASA</td>
+              <td>xxxx</td>
+            </tr>
+          </table>
+          <table border="1" class="deskripsi" cellpadding="20">
+            <tr>
+              <td id="desk">DESKRIPSI SINGKAT</td>
+            </tr>
+            <tr>
+              <td id="isi_desk">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis, nam, autem tempora vel vitae quidem exercitationem
+                iusto omnis corrupti, ipsam laboriosam error asperiores rem non.
+                Cupiditate quas in voluptate nesciunt omnis laboriosam eligendi
+                quidem nemo eaque quo quaerat alias tempora illum sint, fugiat
+                reprehenderit. Modi, dolor voluptatibus? Voluptatibus, ipsam.
+                Deleniti aperiam necessitatibus sequi maiores esse repellat
+                veritatis aliquam. Quaerat dolore temporibus perspiciatis
+                accusantium officiis aut, minus iste aspernatur laborum error
+                quas adipisci excepturi mollitia fuga nesciunt blanditiis
+                beatae? Fugit, deserunt ipsam quod animi dolor nesciunt?
+                Molestias modi maxime soluta explicabo quasi facere debitis quod
+                eum cupiditate aliquid, sint voluptatem obcaecati omnis beatae
+                rem optio repudiandae molestiae culpa laborum cumque saepe.
+                Harum mollitia exercitationem rerum, placeat impedit et
+                repudiandae recusandae deleniti ab officiis, ea minus earum
+                accusantium, necessitatibus explicabo totam quibusdam eos alias
+                quasi! Cupiditate ducimus odit optio accusantium sed nesciunt
+                eos dolorem ad, explicabo officiis eaque alias sunt provident
+                cum, reprehenderit molestias nostrum esse numquam debitis? Quam
+                deserunt expedita sed tempore. Iusto illo rem omnis ullam quod
+                iste nesciunt, aspernatur quas mollitia architecto dolore
+                cupiditate placeat perferendis debitis labore pariatur
+                consequatur aliquid fugit facilis culpa odit illum numquam.
+                Rerum quibusdam porro distinctio id. In enim eos repudiandae
+                minus incidunt excepturi.
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <button class="tombol"><h1>TAMBAH BUKU</h1></button>
+    </div>
+  </body>
+</html>
